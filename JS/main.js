bee0d18029edcf248b6f419307202ce797eb221d
Min Thiha Kyaw <115488903+ArrPound@users.noreply.github.com>
2023-10-05 19:12:39 +0630

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -92,3 +92,42 @@ const tabs = document.querySelectorAll('[data-target]'),
         },
        
       });
+
+      /* ============ scroll section active link =================== */
+
+      const sections = document.querySelectorAll('section[id]')
+
+      function scrollActive() {
+        const scrollY = window.pageYOffset
+
+        sections.forEach(current => {
+            const sectionHeight =current.offsetHeight
+            const sectionTop= current.offsetTop - 50;
+            sectionId =current.getAttribute("id")
+
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            }else{
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+            }
+        })
+      }
+
+      window.addEventListener('scroll', scrollActive)
+
+      
+
+      /* ============== Change background header ========== */
+
+      function scrollHeader() {
+        const nav = document.getElementById('header')
+
+        /// When the scroll is greater than 200 viewport height,
+        /// add the scroll-header 
+
+        if (this.scrollY >= 80 ) nav.classList.add('scroll-header');
+        else nav.classList.remove('scroll-header')
+      }
+
+      window.addEventListener('scroll', scrollHeader)
